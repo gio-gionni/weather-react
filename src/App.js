abc57134bfc862d8e5457301b56c00210017fd32
gio-gionni <g.gazzetto@gmail.com>
2024-12-04 17:40:05 +0100

    primary html

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world</h1>
+        <h1>Weather app</h1>
+        <p>
+          his project was coded by{" "}
+          <a href="https://github.com/gio-gionni">Giulia</a>, and is{" "}
+          <a href="https://github.com/gio-gionni/weather-react">
+            open sourced on GitHub
+          </a>
+        </p>
       </header>
+      <footer></footer>
     </div>
   );
 }
